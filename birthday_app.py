c4291f01201bc0b6af7b5e4e502fc52254592117
AnuranRoy01 <royitachi26@gmail.com>
2025-06-28 21:58:45 +0530

    Update birthday_app.py

--- birthday_app.py
+++ birthday_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from PIL import Image
-import time
 
 # Page config
 st.set_page_config(page_title="Happy Birthday Barnita", layout="wide")
@@ -9,13 +8,19 @@ st.set_page_config(page_title="Happy Birthday Barnita", layout="wide")
 st.markdown("<h1 style='text-align: center; color: hotpink;'>🎉 Happy Birthday Didivalu! 🎉</h1>", unsafe_allow_html=True)
 st.markdown("<h3 style='text-align: center;'>Many Many Happiest Returns of the Day.. 💖</h3>", unsafe_allow_html=True)
 
-# Background music
+# Background music with autoplay using custom HTML
 def add_bg_music(file_path):
-    with open(file_path, "rb") as audio_file:
-        audio_bytes = audio_file.read()
-        st.audio(audio_bytes, format="audio/mp3")
+    st.markdown(
+        f"""
+        <audio autoplay loop>
+            <source src="{file_path}" type="audio/mp3">
+            Your browser does not support the audio element.
+        </audio>
+        """,
+        unsafe_allow_html=True
+    )
 
-add_bg_music("happy-birthday-357371.mp3")  # File must be uploaded to GitHub repo
+add_bg_music("happy-birthday-357371.mp3")  # Make sure this file is uploaded in GitHub
 
 # Image Display Section
 st.markdown("---")
@@ -24,8 +29,8 @@ st.markdown("<h2 style='text-align: center;'>Some Beautiful Memories</h2>", unsa
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    image1 = Image.open("IMG_5365.HEIC")
-    st.image(image1, caption="💐 Barnita & You", use_container_width=True)
+    image1 = Image.open("IMG_5365_converted.PNG")  # <-- Replace this with converted version of the .HEIC file
+    st.image(image1, caption="💐 Me & You", use_container_width=True)
 
 with col2:
     image2 = Image.open("IMG_5368.PNG")
